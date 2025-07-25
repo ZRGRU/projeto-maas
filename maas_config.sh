69e961dc308b900e3b608b013d3ed1436f06455a
Lucas Evangelista <190989128+ZRGRU@users.noreply.github.com>
2025-07-25 12:49:46 -0300

    Adiciona novo pacote para instalação e adiciona um usuário `admin`,. Dados de `email` e `senha` devem ser adicionados via `.env`. Adiciona `env` como modelo.

--- maas_config.sh
+++ maas_config.sh
@@ -2,6 +2,10 @@
 
 # Instalar o MAAS
 sudo snap install maas --channel=3.6/stable
+sudo snap install maas-test-db
 
 # Inicializar o MAAS
 sudo maas init region+rack
+
+# Criar um usuário administrador
+sudo maas createadmin --username=admin --email="${EMAIL}" --password="${PASSWORD}"
